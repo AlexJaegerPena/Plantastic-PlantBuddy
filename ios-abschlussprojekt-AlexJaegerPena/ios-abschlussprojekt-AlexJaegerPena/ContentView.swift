0c8b66852045468773b5fa69442817ff244460eb
Alex <alexjaeger87@live.de>
2025-05-26 10:14:35 +0200

    first commit

---
+++ ios-abschlussprojekt-AlexJaegerPena/ios-abschlussprojekt-AlexJaegerPena/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ios-abschlussprojekt-AlexJaegerPena
+//
+//  Created by Alexandra Jäger on 26.05.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
