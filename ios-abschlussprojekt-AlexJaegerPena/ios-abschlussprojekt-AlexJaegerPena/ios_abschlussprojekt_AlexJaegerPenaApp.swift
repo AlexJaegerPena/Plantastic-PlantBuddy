a8878fdfd386b8be875be1c4cec6fbb21e2cf514
Alex <alexjaeger87@live.de>
2025-05-26 10:14:35 +0200

    first commit

---
+++ ios-abschlussprojekt-AlexJaegerPena/ios-abschlussprojekt-AlexJaegerPena/ios_abschlussprojekt_AlexJaegerPenaApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_abschlussprojekt_AlexJaegerPenaApp.swift
+//  ios-abschlussprojekt-AlexJaegerPena
+//
+//  Created by Alexandra Jäger on 26.05.25.
+//
+
+import SwiftUI
+
+@main
+struct ios_abschlussprojekt_AlexJaegerPenaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
