bb24ecc44e153f1415fb53879c10cb8cccb4a563
Alex <alexjaeger87@live.de>
2025-05-26 11:58:00 +0200

    first commit

---
+++ PlantApp/PlantApp/PlantAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlantAppApp.swift
+//  PlantApp
+//
+//  Created by Alexandra Jäger on 26.05.25.
+//
+
+import SwiftUI
+
+@main
+struct PlantAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
