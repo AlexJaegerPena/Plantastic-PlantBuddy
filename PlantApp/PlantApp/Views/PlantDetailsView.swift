141ddbad8d4dfe9fad8c646b1902807108572b7f
Alex <alexjaeger87@live.de>
2025-05-26 14:59:01 +0200

    .gitignore-Datei hinzufügen

---
+++ PlantApp/PlantApp/Views/PlantDetailsView.swift
@@ -0,0 +1,18 @@
+//
+//  PlantDetailsView.swift
+//  PlantApp
+//
+//  Created by Alexandra Jäger on 26.05.25.
+//
+
+import SwiftUI
+
+struct PlantDetailsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PlantDetailsView()
+}
---
+++ PlantApp/PlantApp/Views/WeatherView.swift
@@ -0,0 +1,18 @@
+//
+//  WeatherView.swift
+//  PlantApp
+//
+//  Created by Alexandra Jäger on 26.05.25.
+//
+
+import SwiftUI
+
+struct WeatherView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WeatherView()
+}
